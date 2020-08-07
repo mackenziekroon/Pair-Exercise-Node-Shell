4bd4170df2ea3e4bc03f2e5b5d9cb2dc0b000d04
Mackenzie Kroon <kroonmackenzie@gmail.com>
2020-08-07 15:20:54 -0500

    final updates

--- bash.js
+++ bash.js
@@ -1,5 +1,26 @@
 process.stdout.write('prompt > ');
 
-const pwd = require('./pwd')
+//const commands = require('./pwd ./ls')
 
-pwd()</
+// pwd();
+// ls();
+
+process.stdin.on('data', (data)=>{
+    const cmd = data.toString().trim();
+
+    if (cmd === 'pwd'){
+        pwd();
+    } 
+    if (cmd === 'ls'){
+        ls();
+    }
+    if (cmd === 'cat') {
+        cat(pwd.js);
+        
+    }
+    
+})
+
+const pwd = require('./pwd')
+const ls = require('./ls')
+const cat = require('./cat')
--- pwd.js
+++ pwd.js
@@ -1,15 +1,8 @@
-
 module.exports = function () {
-    process.stdin.on('data', (data)=>{
-        // const cmd = data.toString().trim();
     
-        // process.stdout.write('You typed: ' + cmd);
-       
-    
-        const cwd = process.cwd();
-        process.stdout.write(cwd);
-    
-         process.stdout.write('\nprompt > ');
-    
-    });
-  }</
+    const cwd = process.cwd();
+    process.stdout.write(cwd);
+
+    process.stdout.write('\nprompt > ');
+
+}
